ee673a8a6c02ab8f3cf558f9b78353bcf2a4862d
klosik007 <pklos1992@gmail.com>
2022-07-12 21:32:07 +0200

    switching to sqlite3
    previous project dependencies removed

--- server/db/mongoose.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
-//mongoose.connect('mongodb://localhost:27017/TdoApp');
-
-module.exports = {mongoose};
