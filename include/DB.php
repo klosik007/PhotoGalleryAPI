93372d3ba7bc6ddb3f92012cd23be0b05bd772d2
Przemysław Kłos <pklos1992@gmail.com>
2023-10-07 22:02:20 +0200

    Database open and return database instance

--- include/DB.php
+++ include/DB.php
@@ -1,8 +1,19 @@
 <?php
 
 namespace PHOTOGALLERY;
+
 abstract class DB {
     public static function open() {
+        $db = new \MySQLi(
+            Settings::$db['host'],
+            Settings::$db['username'],
+            Settings::$db['password'],
+            Settings::$db['database'],
+            Settings::$db['port']
+        );
 
+        $db->query("set names utf8mb4");
+
+        return $db;
     }
 }
